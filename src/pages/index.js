eb23ce6bceeac509265f0df20a88ffdee04dbb15
Tanjung <tanjung.tanjung.1466@gmail.com>
2024-07-24 01:42:54 +0700

    update profile

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,15 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const router = useRouter();
+  useEffect(() => {
+    router.push("/auth/login");
+  }, []);
   return (
     <>
       
